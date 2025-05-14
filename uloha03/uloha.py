1b249c4e3e8eab42c1d6be9ff3878f28064477aa
Petr Šmerda <smerdpet@fit.cvut.cz>
2025-05-14 23:29:05 +0200

    adding hw03, pouze zadani, navod a upresneni zadani pridam zitra

---
+++ uloha03/uloha.py
@@ -0,0 +1,11 @@
+"""
+    Máme za úkol vytvořit Bludiště, ve kterém se bude pohybovat hráč pomocí klávesnice
+    Zdi budou tvořit '#' a volná místa budeme značit jako '.'
+    Hráč bude reprezentovaný pomocí např 'r' (z důvodu že potom přejdeme na algoritmizaci a bude běhat sám, takže ho pak pojmenujeme r jako robot).
+    Budeme zde mít i cíl, který bude značen 'g', jako goal.
+    Hráčem budeme pohybovat skiskem klávesnice, konkrétně písmeny 'w/a/s/d'.
+    Jakmlike bude hráč na poli s 'g', pak se program ukončí a vypíše na obrazovku: Hráč vyhrál!
+"""
+
+
+
